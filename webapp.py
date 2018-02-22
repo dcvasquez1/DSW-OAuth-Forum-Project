5df6d6e8009901c3b1de6c121e2fd8e2d6b3b726
dcvasquez1 <dcvasquez@outlook.com>
2018-02-22 08:56:30 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -8,26 +8,26 @@ import json
 
 app = Flask(__name__)
 
-app.debug = True #Change this to False for production
+app.debug = True # Change this to False for production
 
 app.secret_key = os.environ['SECRET_KEY'] #used to sign session cookies
 oauth = OAuth(app)
 
-#Set up GitHub as OAuth provider
+# Set up GitHub as OAuth provider
 github = oauth.remote_app(
     'github',
-    consumer_key=os.environ['GITHUB_CLIENT_ID'], #your web app's "username" for github's OAuth
-    consumer_secret=os.environ['GITHUB_CLIENT_SECRET'],#your web app's "password" for github's OAuth
-    request_token_params={'scope': 'user:email'}, #request read-only access to the user's email.  For a list of possible scopes, see developer.github.com/apps/building-oauth-apps/scopes-for-oauth-apps
+    consumer_key=os.environ['GITHUB_CLIENT_ID'], # your web app's "username" for github's OAuth
+    consumer_secret=os.environ['GITHUB_CLIENT_SECRET'],# your web app's "password" for github's OAuth
+    request_token_params={'scope': 'user:email'}, # request read-only access to the user's email.  For a list of possible scopes, see developer.github.com/apps/building-oauth-apps/scopes-for-oauth-apps
     base_url='https://api.github.com/',
     request_token_url=None,
     access_token_method='POST',
     access_token_url='https://github.com/login/oauth/access_token',  
-    authorize_url='https://github.com/login/oauth/authorize' #URL for github's OAuth login
+    authorize_url='https://github.com/login/oauth/authorize' # URL for github's OAuth login
 )
 
-#use a JSON file to store the past posts.  A global list variable doesn't work when handling multiple requests coming in and being handled on different threads
-#Create and set a global variable for the name of your JSON file here.  The file will be created on Heroku, so you don't need to make it in GitHub
+# use a JSON file to store the past posts.  A global list variable doesn't work when handling multiple requests coming in and being handled on different threads
+# Create and set a global variable for the name of your JSON file here.  The file will be created on Heroku, so you don't need to make it in GitHub
 
     
 @app.context_processor
@@ -40,14 +40,14 @@ def home():
 
 @app.route('/posted', methods=['POST'])
 def post():
-    #This function should add the new post to the JSON file of posts and then render home.html and display the posts.  
-    #Every post should include the username of the poster and text of the post. 
+    # This function should add the new post to the JSON file of posts and then render home.html and display the posts.  
+    # Every post should include the username of the poster and text of the post. 
     try:
-        with open('posts.json','r+') as f:
+        with open('posts.json', 'r+') as f:
             data = json.load(f)
         username = session['user_data']['login']
         message = request.form['message']
-        data.append({'username':username,'message':message})
+        data.append({'username':username, 'message':message})
         return render_template('home.html', past_posts=posts_to_html())
     except:
         return render_template('home.html', past_posts="<p>Error loading json file or username</p>")
@@ -58,13 +58,13 @@ def posts_to_html():
             data = json.load(f)
         table = Markup("<table> <tr> <th>Username</th> <th>Message</th>")
         for i in data:
-            table.append("<tr> <td>" + i['username'] + "</td>")
-            table.append("<td>" + i['message'] + "</td>")
+            # table.append("<tr> <td>" + i['username'] + "</td>")
+            # table.append("<td>" + i['message'] + "</td>")
         return table
     except:
         return "<p>table failed to load</p>"
     
-#redirect to GitHub's OAuth page and confirm callback URL
+# redirect to GitHub's OAuth page and confirm callback URL
 @app.route('/login')
 def login():   
     return github.authorize(callback=url_for('authorized', _external=True, _scheme='https')) #callback URL must match the pre-configured callback URL
@@ -91,7 +91,7 @@ def authorized():
             message='Unable to login, please try again.  '
     return render_template('message.html', message=message)
 
-#the tokengetter is automatically called to check who is logged in.
+# the tokengetter is automatically called to check who is logged in.
 @github.tokengetter
 def get_github_oauth_token():
     return session.get('github_token')
