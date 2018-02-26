dd504af195c678620447db86326cef145a07a01f
dcvasquez1 <dcvasquez@outlook.com>
2018-02-26 09:16:47 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -49,8 +49,8 @@ def post():
         with open('posts.json', 'r+') as postsFile:
             data = json.load(postsFile)
         username = session['user_data']['login']
-        message = request.form['message']
-        data.append({'username':username, 'message':Post})
+        message = request.form['Post']
+        data.append({'username':username, 'message':message})
         return render_template('home.html', past_posts=posts_to_html())
     except:
         return render_template('home.html', past_posts="<p>Error loading json file or username</p>")
