c85c0b47c86be72ea2604faad72c3a669ada607a
dcvasquez1 <dcvasquez@outlook.com>
2018-03-01 09:19:04 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -62,7 +62,7 @@ def posts_to_html():
     try:
         with open('posts.json', 'r') as jsonPosts:
             data = json.load(jsonPosts)
-        tableString = "<table cellpadding=10> <tr> <th> Username </th> <th> Message </th>"
+        tableString = "<table cellpadding="10"> <tr> <th> Username </th> <th> Message </th>"
         for i in data:
             tableString += " <tr> <td>" + i['username'] + ": </td>"
             tableString += " <td>" + i['message'] + "</td>"
