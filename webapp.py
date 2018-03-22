af75f5950431f9b52feed870cb5d4e888ebc5491
dcvasquez1 <dcvasquez@outlook.com>
2018-03-22 09:34:08 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -55,7 +55,7 @@ def post():
     username = session['user_data']['login']
     message = request.form['message']
 
-    client = pymongo.MongoClient("test_user:18s9h64735f124g5e68@ds213239.mlab.com:13239")
+    client = pymongo.MongoClient("mongodb://test_user:18s9h64735f124g5e68@ds213239.mlab.com:13239")
     db = client["forumapp"]
     posts = db["posts"]
 
@@ -80,7 +80,7 @@ def posts_to_html():
         #    data = json.load(jsonPosts)
         
         tableString = '<table id="postsTable" cellpadding="5"> <tr> <th> Username </th> <th> Message </th> </tr>'
-        client = pymongo.MongoClient("test_user:18s9h64735f124g5e68@ds213239.mlab.com:13239")
+        client = pymongo.MongoClient("mongodb://test_user:18s9h64735f124g5e68@ds213239.mlab.com:13239")
         db = client["forumapp"]
         # collection = db["posts"]
         posts = db["posts"]
